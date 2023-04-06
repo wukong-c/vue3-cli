601a0d7d70487383e45e6badf392ef5f5310627e
caoping <cpgodem@163.com>
2023-04-06 14:33:49 +0800

    chore: 更新包

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,6 @@ import AutoImport from "unplugin-auto-import/vite";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  console.log(mode);
   return {
     base: "./",
     server: {
